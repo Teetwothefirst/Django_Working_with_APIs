5a5d1453abccd2562c231fe4ee8e87c1b3d556d6
Teetwothefirst <dexterdavid835@gmail.com>
2022-07-13 15:30:29 +0100

    I created a model in the model.py file which contains a target url, description, identifier, author etc and I made migrations for the model and added it to the admin site

---
+++ I4G0063927VJ/Readme.md
@@ -0,0 +1,5 @@
+To create migrations for your models use:
+python manage,py makemigrations (appname)
+
+Then to run migrations for everything use 
+python manage.py migrate (appname)
--- I4G0063927VJ/links/models.py
+++ I4G0063927VJ/links/models.py
@@ -1,3 +1,22 @@
+#from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.contrib.auth import get_user_model
+User=get_user_model()
 
 # Create your models here.
+class link(models.Model):
+    # class a_link:
+    #     verbose_name = "link"
+    #     verbose_name_plural = "link"
+    target_url = models.URLField(max_length = 200)
+    #description use models.charfield
+    description = models.CharField(max_length= 200)
+    #identifier use models.slugfield
+    identifier = models.SlugField(max_length = 200, blank = True, unique = True)
+    #author this is a foreign key to current user model use Make use of Django’s get_user_model function.
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="link_posts")
+    # #created_date : A date-time column, use Django’s models.DateTimeField.
+    Created_date = models.DateTimeField()
+    # #active :  A boolean (True or False), determining if the shortened URL is publicly accessible. Make use of Django’s BooleanField. The default should be True.
+    active = models.BooleanField(default = True)
+
