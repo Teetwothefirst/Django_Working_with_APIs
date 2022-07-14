dcb4ccd76890f430b553be95b71e193634aabfb3
Teetwothefirst <dexterdavid835@gmail.com>
2022-07-14 17:21:12 +0100

    Updated the settings and admin files

--- I4G0063927VJ/links/admin.py
+++ I4G0063927VJ/links/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+from .models import link
+
 
 # Register your models here.
-admin.site.register("link")
+class linkupDB(admin.ModelAdmin):
+    list_display = [
+        "target_url","description","identifier", "author", "Created_date","active"
+    ]
+admin.site.register(link, linkupDB)
